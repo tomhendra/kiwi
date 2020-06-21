01cde5a63688dfef77f65ed8416efe2c019d189f
Thomas Hendra <tom.hendra@outlook.com>
2020-06-21 11:44:18 +0200

    feat(config): Ant Design setup

--- buho-app/src/App.test.tsx
+++ buho-app/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Click Me button', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const buttonElement = getByText(/Click Me/i);
+  expect(buttonElement).toBeInTheDocument();
 });
