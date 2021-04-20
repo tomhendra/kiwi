a8b0321134984d74e53ec6507a7610a7cfdc9485
Tom Hendra <tom.hendra@outlook.com>
2021-04-20 17:39:18 +0200

    refactor(api): useAsync hook types

--- src/core/hooks/use-async.tsx
+++ src/core/hooks/use-async.tsx
@@ -1,48 +1,64 @@
 import * as React from 'react';
 
-function useSafeDispatch(dispatch: React.Dispatch<any>) {
+/*
+ * Example usage:
+ * const {data, error, status, run} = useAsync()
+ * React.useEffect(() => {
+ *  run(fetchPokemon(pokemonName))
+ * }, [pokemonName, run])
+ */
+
+type Status = 'idle' | 'pending' | 'rejected' | 'resolved';
+type Data = any; // TODO: add actual data types once defined
+
+interface State {
+  status: Status;
+  data: Data | null;
+  error: Error | null;
+}
+
+function useSafeDispatch(dispatch: React.Dispatch<State>) {
   const mounted = React.useRef(false);
+
   React.useLayoutEffect(() => {
     mounted.current = true;
     return () => {
       mounted.current = false;
     };
   }, []);
+
   return React.useCallback(
-    (...args) => (mounted.current ? dispatch(args) : void 0),
+    args => (mounted.current ? dispatch(args) : void 0),
     [dispatch],
   );
 }
 
-/* 
-  Example usage:
-  const {data, error, status, run} = useAsync()
-  React.useEffect(() => {
-    run(fetchPokemon(pokemonName))
-  }, [pokemonName, run])
-*/
+const defaultInitialState: State = { status: 'idle', data: null, error: null };
 
-const defaultInitialState = { status: 'idle', data: null, error: null };
-
-function useAsync(initialState?: any) {
+function useAsync(initialState?: State) {
   const initialStateRef = React.useRef({
     ...defaultInitialState,
     ...initialState,
   });
 
   const [{ status, data, error }, setState] = React.useReducer(
-    (s: any, a: any) => ({ ...s, ...a }),
+    // useReducer accepts 2 args.
+    // the first is the reducer function which is called with...
+    // s: the current state
+    // a: whatever it is that the dispatch function (setState) is called with, often called an "action"
+    (currentState: State, action: State) => ({ ...currentState, ...action }),
+    // the second is the current state...
     initialStateRef.current,
   );
 
   const safeSetState = useSafeDispatch(setState);
 
   const setData = React.useCallback(
-    data => safeSetState({ data, status: 'resolved' }),
+    (data: Data) => safeSetState({ data, status: 'resolved' }),
     [safeSetState],
   );
   const setError = React.useCallback(
-    error => safeSetState({ error, status: 'rejected' }),
+    (error: Error) => safeSetState({ error, status: 'rejected' }),
     [safeSetState],
   );
   const reset = React.useCallback(() => safeSetState(initialStateRef.current), [
@@ -53,16 +69,16 @@ function useAsync(initialState?: any) {
     promise => {
       if (!promise || !promise.then) {
         throw new Error(
-          `The argument passed to useAsync().run must be a promise. Maybe a function that's passed isn't returning anything?`,
+          `The argument passed to useAsync().run must be a promise. Maybe a function that's been passed isn't returning anything?`,
         );
       }
       safeSetState({ status: 'pending' });
       return promise.then(
-        (data: any) => {
+        (data: Data) => {
           setData(data);
           return data;
         },
-        (error: any) => {
+        (error: Error) => {
           setError(error);
           return Promise.reject(error);
         },
--- src/screens/dashboard.tsx
+++ src/screens/dashboard.tsx
@@ -3,65 +3,15 @@ import * as React from 'react';
 import { useAsync } from 'core/hooks/use-async';
 import { API, graphqlOperation } from 'aws-amplify';
 import { listItems } from 'core/graphql';
-import { Item, ListItemsQuery } from 'core/models';
+import { Item } from 'core/models';
 import { theme } from 'core/theme';
 
 function Dashboard() {
-  const {
-    isLoading,
-    isError,
-    isSuccess,
-    error,
-    status,
-    data,
-    run,
-  } = useAsync();
+  const { isLoading, isError, isSuccess, error, data, run } = useAsync();
 
   React.useEffect(() => {
     run(API.graphql(graphqlOperation(listItems)));
-    // fetchItems();
   }, [run]);
-
-  console.log({ data, error, status, run, isLoading, isError, isSuccess });
-
-  // async function fetchItems() {
-  //   try {
-  //     const itemData: any = await API.graphql(graphqlOperation(listItems));
-  //     setItems(itemData.data.listItems.items);
-  //   } catch (err) {
-  //     console.log('error fetching items');
-  //   }
-  // }
-
-  // if (isLoading) {
-  //   return (
-  //     <div
-  //       css={{
-  //         padding: '3rem',
-  //         borderRadius: theme.radii.md,
-  //         background: theme.colors.white,
-  //         marginTop: theme.space[5],
-  //       }}
-  //     >
-  //       <p>Loading...</p>
-  //     </div>
-  //   );
-  // }
-
-  // if (isError) {
-  //   return (
-  //     <div
-  //       css={{
-  //         padding: '3rem',
-  //         borderRadius: theme.radii.md,
-  //         background: theme.colors.white,
-  //         marginTop: theme.space[5],
-  //       }}
-  //     >
-  //       <pre>{error}</pre>
-  //     </div>
-  //   );
-  // }
 
   return (
     <div
@@ -72,11 +22,12 @@ function Dashboard() {
         marginTop: theme.space[5],
       }}
     >
-      {isSuccess
-        ? data?.data?.listItems?.items?.map((item: Item) => (
-            <p key={item.id}>{item.title}</p>
-          ))
-        : null}
+      {isLoading && <p>Loading...</p>}
+      {isError && <pre>{error}</pre>}
+      {isSuccess &&
+        data?.data?.listItems?.items?.map((item: Item) => (
+          <p key={item.id}>{item.title}</p>
+        ))}
     </div>
   );
 }
