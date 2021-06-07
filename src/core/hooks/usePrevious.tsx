80ad71a421759cb86225f83d9af3ae265f04a6b8
Tom Hendra <tom.hendra@outlook.com>
2021-06-07 18:06:36 +0200

    feat(hooks): usePrevious hook

---
+++ src/core/hooks/usePrevious.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+function usePrevious(value: any) {
+  const ref = React.useRef();
+  React.useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
+
+export { usePrevious };
